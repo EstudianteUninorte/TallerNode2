b02aeef376fa2e2c2a4e0b6ad8499091376d0c9c
Roberto Campy <rocacav@gmail.com>
2020-07-24 18:07:48 -0500

    Se realiza método de consulta de tweets mas comentados.

--- api/routers/tweets/index.js
+++ api/routers/tweets/index.js
@@ -10,6 +10,8 @@ router.route('/')
 router.route('/comment').post(controller.newComment);
 router.route('/:id').get(controller.getTweet);
 
+router.route('/top/:count').get(controller.commentsTopTweets);
+
 router.route('/comments').delete(controller.deleteComment);
 router.route('/lasts/:count').get(controller.lastTweets);
 router.route('/top/commenters/:count').get(controller.usersTopTweets);
